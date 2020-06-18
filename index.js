83a4ba7f25ef4506d825762c58d208fc80baf047
Rajput Rishabh Tanwar #rrt <rishabhtanwar2000@gmail.com>
2020-06-18 12:38:29 +0530

    provide links

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 var card=document.querySelectorAll(".project");
 var front=document.querySelectorAll(".project .front .projectimage");
 var content=document.querySelectorAll(".project .content");
-var title=document.querySelectorAll(".project .front .title")
+var title=document.querySelectorAll(".project .front .title");
+var tech=document.querySelectorAll(".project .front .tech");
 
 for(let i=0;i<card.length;i++){
     card[i].addEventListener('mouseover',() =>{
        front[i].style.opacity='0';
+       tech[i].style.opacity='0';
        content[i].style.opacity='1';
        content[i].style.transition=' opacity 0.3s ease-in 0.3s';
        front[i].style.transition='opacity 0.3s ease-in 0.0s';
+       tech[i].style.transition='opacity 0.3s ease-in 0.0s';
        title[i].style.transition='0.3s ease-in 0.2s';
-       title[i].style.top='-200px';
-       title[i].style.left='20px';
+       title[i].style.top='-170px';
+       title[i].style.left='15px';
     })
     card[i].addEventListener('mouseout',() =>{
         front[i].style.opacity='1';
+        tech[i].style.opacity='1';
         content[i].style.opacity='0';
-        front[i].style.transition ='opacity 0.3s ease-in 0.3s';        
+        front[i].style.transition ='opacity 0.3s ease-in 0.3s'; 
+        tech[i].style.transition ='opacity 0.3s ease-in 0.3s';        
         content[i].style.transition='opacity 0.3s ease-in 0.0s';
         title[i].style.top='0px';
         title[i].style.left='0px';        
         title[i].style.transition='0.3s ease-in 0.2s ';
      })
 }
-// let submit=document.querySelector(".submit");
-// submit.addEventListener("click",()=> {
-//    console.log("hh");
-// })
- function submit(){
-    
- }
+let container=document.querySelector(".contact-container form .submit-container");
+let submit=document.querySelector(".submit");
+submit.addEventListener("click",()=> {
+   container.innerHTML=`
+   <div class="submit-container">
+   <p class="onsubmit">Your message has been sent!Thank you :)</p>
+   <a class="aftersubmit submit">Submit</a>
+   </div> 
+   `;
+})
