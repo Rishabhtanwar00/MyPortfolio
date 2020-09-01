0ab1af6e56f246bfced4ee1d4572e6390b4c1ea7
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2020-09-02 00:03:58 +0530

    improving design and adding animations

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
 var card = document.querySelectorAll('.project');
-var front = document.querySelectorAll('.project .front .projectimage');
+var projectImage = document.querySelectorAll('.project .projectimage');
 var content = document.querySelectorAll('.project .content');
-var title = document.querySelectorAll('.project .front .title');
-var tech = document.querySelectorAll('.project .front .tech');
 
 for (let i = 0; i < card.length; i++) {
   card[i].addEventListener('mouseover', () => {
-    front[i].style.opacity = '0';
-    tech[i].style.opacity = '0';
+    projectImage[i].style.opacity = '0';
     content[i].style.opacity = '1';
-    content[i].style.transition = ' opacity 0.3s ease-in 0.3s';
-    front[i].style.transition = 'opacity 0.3s ease-in 0.0s';
-    tech[i].style.transition = 'opacity 0.3s ease-in 0.0s';
-    title[i].style.transition = '0.3s ease-in 0.2s';
-    title[i].style.top = '-170px';
-    title[i].style.left = '15px';
+    content[i].style.transition = ' opacity 0.1s ease-in';
   });
   card[i].addEventListener('mouseout', () => {
-    front[i].style.opacity = '1';
-    tech[i].style.opacity = '1';
+    projectImage[i].style.opacity = '1';
     content[i].style.opacity = '0';
-    front[i].style.transition = 'opacity 0.3s ease-in 0.3s';
-    tech[i].style.transition = 'opacity 0.3s ease-in 0.3s';
-    content[i].style.transition = 'opacity 0.3s ease-in 0.0s';
-    title[i].style.top = '0px';
-    title[i].style.left = '0px';
-    title[i].style.transition = '0.3s ease-in 0.2s ';
+    projectImage[i].style.transition = 'opacity 0.3s ease-in';
   });
 }
 let container = document.querySelector(
