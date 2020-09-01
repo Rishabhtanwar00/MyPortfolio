704e639dcc6015ca23ba911bb31b0418e8f7caeb
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2020-09-01 14:05:01 +0530

    updating project link

--- index.js
+++ index.js
@@ -65,4 +65,3 @@ hamgurger.addEventListener('click', () => {
   }
 });
 document.querySelectorAll('.button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
-
